b93d33b477dc0719b882e631fbe6582e953139e6
yangmin <yangmin@chuchujie.com>
2017-11-15 16:14:44 +0800

    test

--- first/pages/index/index.js
+++ first/pages/index/index.js
@@ -5,17 +5,14 @@ const app = getApp()
 Page({
     data: {
         scrollH: 0, // scrollview滚动高度
-        loadingCount: 0,
-        images: [],
-        col1: [],
-        col2: [],
-        column:[[],[]], // 展示数据
+        column:[], // 展示数据
         ww: 0,      // 显示区总宽度
         wh: 0,      // 显示区总高度
         columnWidth:0,   // 每列的宽度
         columnNum: 10,   // 每次加载的总数据
         columnNumber: 2, // 列数
         indexImage: 0 ,  // 第几条数据
+        isloading: false, // 是否加载中
     },
     onLoad: function () {
         var that = this;
@@ -23,16 +20,19 @@ Page({
             success: (res) => {
                 that.data.ww = res.windowWidth;// 显示区总宽度
                 that.data.wh = res.windowHeight; // 显示区总高度
-                that.data.columnWidth = res.windowWidth / that.data.columnNumber ; // 每列的宽度
-                that.data.scrollH = res.windowHeight; // 列表滚动区域的高度
+                that.setData({
+                    columnWidth: res.windowWidth / that.data.columnNumber , // 每列的宽度
+                    scrollH: res.windowHeight + 'px'  // 列表滚动区域的高度
+                })
                 //加载首组图片
                 that.waterFall()
             }
         })
     },
     waterFall:function(){
-        var htmlColumn = [[],[]], self = this;
+        var htmlColumn = [], self = this;
         for (let i = 0, iLen = self.data.columnNumber; i < iLen; i++){
+            htmlColumn.push([]);
             for (let j = 0, jLen = self.data.columnNum / self.data.columnNumber; j < jLen ; j++){
                 self.data.indexImage = i + self.data.columnNumber * j;
                 var index = self.getIndex();
@@ -43,14 +43,38 @@ Page({
             }
         }
         var templist = self.data.column;
-        templist[0] = templist[0].concat(htmlColumn[0]);
-        templist[1] = templist[1].concat(htmlColumn[1]);
+        templist[0] = templist[0] ? templist[0].concat(htmlColumn[0]) : htmlColumn[0];
+        templist[1] = templist[1] ? templist[1].concat(htmlColumn[1]) : htmlColumn[1];
         self.setData({
-            column: self.data.column.concat(templist)
+            column: templist
         })
+        self.data.isloading = false;
     },
+    scrolling:function(e){
+        this.scrollTop = e.detail.scrollTop;
+    },
+    imgScroll:function(){
+        let self = this;
+        if(!self.isloading){
+            self.isloading = true;
+            self.appendImg();
+        }
+    },
+    appendImg:function(){
+        let htmlColumn = this.data.column,that = this;
+        for (let start = 0; start < this.columnNumber; start++) {
+            if (htmlColumn[start] && !that.isloading){
+                // if()
+            }
 
-
+            var eleColumn = document.getElementById("waterFallColumn_" + start);
+            if (eleColumn && !this.loadFinish) {
+                if (eleColumn.offsetTop + eleColumn.clientHeight < this.scrollTop + (window.innerHeight || document.documentElement.clientHeight)) {
+                    this.append(eleColumn);
+                }
+            }
+        }
+    },
 
     // 返回固定格式的图片名
     getIndex: function () {
@@ -62,5 +86,16 @@ Page({
         }
         return index;
     },
-
+    /**
+     * @imgLoad 瀑布流中图片加载完成事件
+     */
+    imgLoad:function(e){
+        let oImgW = e.detail.width;
+        let oImgH = e.detail.height;
+        let imgH = oImgH * 345 / oImgW;
+        this.data.column[e.target.dataset.columnindex][e.target.dataset.imgindex].height = imgH + 'rpx';
+        this.setData({
+            column: this.data.column
+        })
+    }
 })
